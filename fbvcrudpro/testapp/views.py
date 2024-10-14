cb91b612fa3e5a246073248ea036f9456e4ac9c6
veeresh-vla <machaniveeresh55@gmail.com>
2024-10-14 15:57:10 +0530

    commit

---
+++ fbvcrudpro/testapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from testapp.models import Employee
+from testapp.forms import EmployeeForm
+from django.shortcuts import redirect
+
+# Create your views here.
+
+def retrieve_view(request):#READ
+    emp_list = Employee.objects.all()
+    return render(request,'testapp/index.html',{'emp_list':emp_list})
+
+def insert_view(request):#CREATE
+    form = EmployeeForm()
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    return render(request,'testapp/insert.html',{'form':form})
+ 
+def update_view(request,id):#UPDATE
+    employee = Employee.objects.get(id = id)
+    form = EmployeeForm(instance=employee)
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST,instance=employee)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    return render(request,'testapp/update.html',{'form':form})
+
+def delete_view(request,id):#DELETE
+    employee = Employee.objects.get(id = id)
+    employee.delete()
+    return redirect('/')
